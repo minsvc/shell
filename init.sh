ccdd42511c7be2642f9d84c8bf05202457fd17e3
zcl <44311530+minsvc@users.noreply.github.com>
2018-11-17 23:42:18 +0800

    Add file via upload

---
+++ init.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+sed -i -e 's/SELINUX=enforcing/SELINUX=disabled/'
+
+systemctl stop firewalld
+systemctl disabled firewalld
+
+sudo yum install -y yum-utils   device-mapper-persistent-data   lvm2
+sudo yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce
+
+systemctl start docker
+systemctl enable docker
+docker run --restart always -e PASSWORD=yourpassword -e METHOD=aes-256-cfb -p 9992:8388 -p 9992:8388/udp -d shadowsocks/shadowsocks-libev
+
+
+
