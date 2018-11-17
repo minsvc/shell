b1175ba5afbb04119bdb20cf3e9993c075876c89
zcl <44311530+minsvc@users.noreply.github.com>
2018-11-18 00:16:34 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,7 +7,7 @@ systemctl disabled firewalld
 
 sudo yum install -y yum-utils   device-mapper-persistent-data   lvm2
 sudo yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 
 systemctl start docker
 systemctl enable docker
